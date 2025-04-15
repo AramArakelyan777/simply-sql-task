9d154d560378aed179ce83681506be58a90e1922
Aram Arakelyan <aram777arakelyan@gmail.com>
2025-04-15 19:15:14 +0400

    count cars task done

--- commands.sql
+++ commands.sql
@@ -12,3 +12,31 @@ GROUP BY
     makes.name
 ORDER BY
     average_price DESC;
+
+-- Find the oldest and newest car years for each make.
+SELECT
+    makes.id,
+    makes.name,
+    min(cars.year) AS oldest,
+    max(cars.year) AS newest
+FROM
+    makes
+    INNER JOIN models ON makes.id = models.make_id
+    INNER JOIN cars ON models.id = cars.model_id
+GROUP BY
+    makes.name,
+    makes.id
+ORDER BY
+    makes.id;
+
+-- List models with more than 1 car, along with the count of cars.
+SELECT
+    name,
+    COUNT(models.name) AS `count`
+FROM
+    models
+    INNER JOIN cars ON models.id = cars.model_id
+GROUP BY
+    models.name
+HAVING
+    `count` > 1;
